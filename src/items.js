bd51c7e439d512285944a44afd6ddd13cde6a34c
Saad Hussain <saadcfi4@gmail.com>
2025-07-18 16:54:26 +0400

    chaned product name

--- src/items.js
+++ src/items.js
@@ -1,6 +1,6 @@
 const products = [
   { id: 1, name: "iPhone 14", price: 799 },
-  { id: 2, name: "Samsung Galaxy S23", price: 499 },
+  { id: 2, name: "Galaxy S23", price: 499 },
   { id: 3, name: "CM Punk T-shirt", price: 782 },
 ];
 
